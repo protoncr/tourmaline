c9ca090a617d30d11b40b9fa22738ff8880096c8
Chris Watson <chris@watzon.me>
2018-02-13 17:43:43 -0700

    Added ngrok example

---
+++ examples/ngrok_webgook.cr
@@ -0,0 +1,17 @@
+require "ngrok"
+require "../src/tourmaline"
+
+Ngrok.start({ addr: "127.0.0.1:3400" }) do |ngrok|
+
+  bot = Tourmaline::Bot::Client.new(ENV["API_KEY"])
+
+  bot.command("echo") do |message, params|
+    text = params.join(" ")
+    bot.send_message(message.chat.id, text)
+    bot.delete_message(message.chat.id, message.message_id)
+  end
+
+  bot.set_webhook(ngrok.ngrok_url_https)
+  bot.serve("127.0.0.1", 3400)
+
+end
