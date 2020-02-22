cd831cbf2cdba8fb8b2019d70cbe28dd8080d8f0
Chris Watson <cawatson1993@gmail.com>
2020-02-21 21:24:03 -0700

    Fix EchoBot example

--- examples/echo_bot.cr
+++ examples/echo_bot.cr
@@ -3,18 +3,9 @@ require "../src/tourmaline"
 class EchoBot < Tourmaline::Client
   include Tourmaline
 
-  def initialize(api_key)
-    super(api_key)
-  end
-
   @[Command("echo")]
   def echo_command(ctx)
     ctx.reply(ctx.text)
-  end
-
-  @[Hears(/hello/i)]
-  def on_message(ctx)
-    ctx.reply("Hi there")
   end
 end
 
