3912eff3a10a39d3e3f5129553e998a5bc6aaa40
Chris Watzon <cawatson1993@gmail.com>
2020-04-10 22:21:53 -0600

    Fix FilterBot name

--- examples/filter_bot.cr
+++ examples/filter_bot.cr
@@ -1,9 +1,9 @@
 require "../src/tourmaline"
 
-class EchoBot < Tourmaline::Client
+class FilterBot < Tourmaline::Client
   HelloFilter = RegexFilter.new(/hello/) | RegexFilter.new(/hi/)
 
-  @[On(:text, EchoBot::HelloFilter)]
+  @[On(:text, FilterBot::HelloFilter)]
   def on_hello(ctx)
     if message = ctx.message
       message.reply("https://nohello.com")
