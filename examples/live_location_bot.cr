70279aadbd337261e9126789974bf95dba0b7936
Chris Watson <cawatson1993@gmail.com>
2020-06-03 17:17:48 -0600

    Use new errors to stop live location

--- examples/live_location_bot.cr
+++ examples/live_location_bot.cr
@@ -3,15 +3,18 @@ require "../src/tourmaline"
 class LiveLocationBot < Tourmaline::Client
   @[Command("start")]
   def send_live_location(client, update)
-    lat = 40.7608
-    lon = 111.8910
-    message = update.message.try &.reply_with_location(lat, lon, live_period: 60)
-    60.times do
-      lat += rand * 0.001
-      lon += rand * 0.001
-      message.edit_live_location(lat, lon)
-      sleep(1)
+    if message = update.message
+      lat = 40.7608
+      lon = 111.8910
+      loc = message.reply_with_location(lat, lon, live_period: 60)
+      loop do
+        lat += rand * 0.0001
+        lon += rand * 0.0001
+        loc.edit_live_location(lat, lon)
+        sleep(5)
+      end
     end
+  rescue Error::MessageCantBeEdited
   end
 end
 
