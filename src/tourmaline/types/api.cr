2852b44332f64c5c49530f7a284dd0c06e8be27e
Chris Watson <cawatson1993@gmail.com>
2024-01-16 01:07:48 +0000

    bot api update

--- src/tourmaline/types/api.cr
+++ src/tourmaline/types/api.cr
@@ -1913,7 +1913,7 @@ module Tourmaline
     # Optional. The placeholder to be shown in the input field when the keyboard is active; 1-64 characters
     property input_field_placeholder : String | ::Nil
 
-    # Optional. Use this parameter if you want to show the keyboard to specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message. Example: A user requests to change the bot's language, bot replies to the request with a keyboard to select the new language. Other users in the group don't see the keyboard.
+    # Optional. Use this parameter if you want to show the keyboard to specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message. Example: A user requests to change the bot's language, bot replies to the request with a keyboard to select the new language. Other users in the group don't see the keyboard.
     property? selective : Bool | ::Nil
 
     def initialize(
@@ -2054,7 +2054,7 @@ module Tourmaline
     # Requests clients to remove the custom keyboard (user will not be able to summon this keyboard; if you want to hide the keyboard from sight but keep it accessible, use one_time_keyboard in ReplyKeyboardMarkup)
     property? remove_keyboard : Bool
 
-    # Optional. Use this parameter if you want to remove the keyboard for specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message. Example: A user votes in a poll, bot returns confirmation message in reply to the vote and removes the keyboard for that user, while still showing the keyboard with poll options to users who haven't voted yet.
+    # Optional. Use this parameter if you want to remove the keyboard for specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message. Example: A user votes in a poll, bot returns confirmation message in reply to the vote and removes the keyboard for that user, while still showing the keyboard with poll options to users who haven't voted yet.
     property? selective : Bool | ::Nil
 
     def initialize(
@@ -2229,7 +2229,7 @@ module Tourmaline
     # Optional. The placeholder to be shown in the input field when the reply is active; 1-64 characters
     property input_field_placeholder : String | ::Nil
 
-    # Optional. Use this parameter if you want to force reply from specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
+    # Optional. Use this parameter if you want to force reply from specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
     property? selective : Bool | ::Nil
 
     def initialize(
